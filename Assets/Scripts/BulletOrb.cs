f6cf2d803fc41b0b2ce4ce154d99ca9491223ae8
JavaBeginner66 <42007131+JavaBeginner66@users.noreply.github.com>
2020-03-16 23:12:12 +0100

    bulletHell prototype done

--- Assets/Scripts/BulletOrb.cs
+++ Assets/Scripts/BulletOrb.cs
@@ -5,10 +5,30 @@ using UnityEngine;
 public class BulletOrb : MonoBehaviour
 {
     public float scaleSpeed;
-    // Update is called once per frame
+    public Transform[] orbPoints;
+    public GameObject[] orbs;
+
+    public GameObject orbPrefab;
+
+    private void Start()
+    {
+        orbPoints = new Transform[transform.childCount];
+        orbs = new GameObject[orbPoints.Length];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            orbPoints[i] = transform.GetChild(i);
+            orbs[i] =  Instantiate(orbPrefab);
+        }
+
+    }
     void Update()
     {
         if(transform.localScale.x < 30f)
             transform.localScale += new Vector3(scaleSpeed * Time.deltaTime, 0f, scaleSpeed * Time.deltaTime);
+
+        for (int i = 0; i < orbPoints.Length; i++)
+        {
+            orbs[i].transform.position = orbPoints[i].transform.position;
+        }
     }
 }
