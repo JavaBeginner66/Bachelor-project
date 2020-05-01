b1a04071f5afbfdbedda7f60798ffcc9fa7d2c6e
JavaBeginner66 <42007131+JavaBeginner66@users.noreply.github.com>
2020-05-01 19:49:16 +0200

    Score menu placeholder

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,8 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public void Play()
+    {
+        SceneManager.LoadScene(0);
+    }
     
+    public void Exit()
+    {
+        Application.Quit();
+    }
 }
